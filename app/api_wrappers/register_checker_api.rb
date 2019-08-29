d0d51a084ea42ffca2ee737c2bc5ab69fa5d3ea4
Wojtek Mikusek <w.mikusek@exlabs.pl>
2019-08-29 11:54:10 +0200

    [NT] Add logs to RegisterCheckerApi (#8)

--- app/api_wrappers/register_checker_api.rb
+++ app/api_wrappers/register_checker_api.rb
@@ -5,23 +5,22 @@ class RegisterCheckerApi < BaseApi
 
   class << self
     def register_job(file_name, correlation_id)
-      params = {
-        "filename": file_name,
-        "s3Bucket": bucket_name
-      }.to_json
-
+      log_call("Registering job with file name: #{file_name}")
       response = request(:post, "#{base_path}/jobs",
-                         body: params, headers: custom_headers(correlation_id))
+                         body: register_body(file_name),
+                         headers: custom_headers(correlation_id))
       response['jobName']
     end
 
     def job_status(job_uuid, correlation_id)
+      log_call("Getting job status with job uuid: #{job_uuid}")
       response = request(:get, "#{base_path}/jobs/#{job_uuid}",
                          headers: custom_headers(correlation_id))
       response['status']
     end
 
     def job_errors(job_uuid, correlation_id)
+      log_call("Getting job errors with job uuid: #{job_uuid}")
       response = request(:get, "#{base_path}/jobs/#{job_uuid}",
                          headers: custom_headers(correlation_id))
       return nil unless response['status'] == 'FAILURE'
@@ -31,8 +30,11 @@ class RegisterCheckerApi < BaseApi
 
     private
 
-    def bucket_name
-      ENV['S3_AWS_BUCKET']
+    def register_body(file_name)
+      {
+        "filename": file_name,
+        "s3Bucket": ENV['S3_AWS_BUCKET']
+      }.to_json
     end
 
     def custom_headers(correlation_id)
@@ -44,6 +46,10 @@ class RegisterCheckerApi < BaseApi
 
     def base_path
       '/v1/retrofit/register-csv-from-s3'
+    end
+
+    def log_call(msg)
+      Rails.logger.info "[RegisterCheckerApi] #{msg}"
     end
   end
 end
