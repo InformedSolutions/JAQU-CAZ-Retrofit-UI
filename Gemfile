ab347ab8d7cda2ab9dd3318d4136fe43c9fd27ed
Sergii Chub <s.chub@exlabs.pl>
2020-03-04 13:35:58 +0100

    [CAZB-639] Content change to Retrofit Cookie Policy (#97)

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ group :development, :test do
 end
 
 group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
   gem 'listen'
   gem 'spring'
   gem 'spring-watcher-listen'
--- spec/api_wrappers/register_checker_api_spec.rb
+++ spec/api_wrappers/register_checker_api_spec.rb
@@ -14,8 +14,11 @@ RSpec.describe RegisterCheckerApi do
     before do
       stub_request(:post, %r{register-csv-from-s3/jobs})
         .with(
-          headers: { 'Content-Type' => 'application/json', 'X-Correlation-ID' => correlation_id },
-          body: { "filename": filename, "s3Bucket": 'S3_AWS_BUCKET' }.to_json
+          headers: headers,
+          body: {
+            'filename': filename,
+            's3Bucket': ENV.fetch('S3_AWS_BUCKET', 'S3_AWS_BUCKET')
+          }.to_json
         ).to_return(status: 201, body: { jobName: job_name }.to_json)
     end
 
@@ -62,5 +65,11 @@ RSpec.describe RegisterCheckerApi do
         end
       end
     end
+  end
+
+  private
+
+  def headers
+    { 'Content-Type' => 'application/json', 'X-Correlation-ID' => correlation_id }
   end
 end
