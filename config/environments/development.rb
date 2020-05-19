816f5437cfa7e150c6529964dffb6c3c6e6e8698
Bartosz Tkaczewski <bartosz.tkaczewski@gmail.com>
2020-05-19 08:48:04 +0200

    [CAZ-2579] Update due to CR comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,5 +62,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  # Use to have access to the application via custom DNS localy
   config.hosts << 'retrofit-ui.caz'
 end
