1511d803469fc4afd220ab03b1969a520bd89119
Sergii Chub <s.chub@exlabs.pl>
2020-06-08 16:09:43 +0200

    Ecs deploy changes (#139)

--- config/initializers/amazon_s3.rb
+++ config/initializers/amazon_s3.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 # :nocov:
-unless ENV['SKIP_AWS_CREDENTIALS']
-  credentials = if ENV['S3_AWS_ACCESS_KEY_ID']
-                  Aws::Credentials.new(
-                    ENV.fetch('S3_AWS_ACCESS_KEY_ID', 'S3_AWS_ACCESS_KEY_ID'),
-                    ENV.fetch('S3_AWS_SECRET_ACCESS_KEY', 'S3_AWS_SECRET_ACCESS_KEY')
-                  )
-                else
-                  Aws::ECSCredentials.new({ ip_address: '169.254.170.2' })
-                end
+credentials = if ENV['S3_AWS_ACCESS_KEY_ID']
+                Aws::Credentials.new(
+                  ENV.fetch('S3_AWS_ACCESS_KEY_ID', 'S3_AWS_ACCESS_KEY_ID'),
+                  ENV.fetch('S3_AWS_SECRET_ACCESS_KEY', 'S3_AWS_SECRET_ACCESS_KEY')
+                )
+              else
+                Aws::ECSCredentials.new({ ip_address: '169.254.170.2' })
+              end
 
-  AMAZON_S3_CLIENT = Aws::S3::Resource.new(
-    region: ENV.fetch('S3_AWS_REGION', 'S3_AWS_REGION'),
-    credentials: credentials
-  )
-end
+AMAZON_S3_CLIENT = Aws::S3::Resource.new(
+  region: ENV.fetch('S3_AWS_REGION', 'S3_AWS_REGION'),
+  credentials: credentials
+)
 # :nocov:
