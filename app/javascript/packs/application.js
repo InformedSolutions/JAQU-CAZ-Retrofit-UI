254ecb620909e5fb8690f4c93ec9e831bbe1107d
proctorj <james.proctor@informed.com>
2021-05-25 13:49:37 +0100

    Removed changes not needed for retrofit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,10 +16,8 @@ import '../styles/application.scss';
 import '../src/GovUKAssets';
 import { initAll } from 'govuk-frontend/govuk/all.js';
 import cookieControl from "../src/cookieControl";
-import initBackLink from "../src/backLink/init";
 
 document.body.classList.add('js-enabled');
 
 initAll();
 cookieControl();
-initBackLink();
